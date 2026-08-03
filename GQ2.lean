f470480d62e0bd1da506faef8368d7174f95cd35
David Roe <roed.math@gmail.com>
2026-08-03 15:20:38 -0400

    Export L word coefficient connecting map

--- GQ2.lean
+++ GQ2.lean
@@ -332,6 +332,7 @@ import GQ2.Dyadic.Instances.LSourceComparison
 import GQ2.Dyadic.Instances.LSourceComparisonCard
 import GQ2.Dyadic.Instances.LFlexibleH2
 import GQ2.Dyadic.Instances.LFlexibleH2Naturality
+import GQ2.Dyadic.Instances.LWordDelta
 import GQ2.Dyadic.Instances.LComparisonSquares
 import GQ2.Dyadic.Instances.LSourceEulerCard
 import GQ2.Dyadic.Instances.LSourceDualityAdapter
