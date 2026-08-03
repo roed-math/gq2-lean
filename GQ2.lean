8f0a41bbbaaa6d7c9e570f539cafdc9a3a49760d
David Roe <roed.math@gmail.com>
2026-08-03 15:36:20 -0400

    Import direct simple H2 surjectivity theorem

--- GQ2.lean
+++ GQ2.lean
@@ -358,6 +358,7 @@ import GQ2.Dyadic.Instances.GammaLRelatorRealizationQTwo
 import GQ2.Dyadic.Instances.GammaLTateDirect
 import GQ2.Dyadic.Instances.GammaLTateProviderCore
 import GQ2.Dyadic.Instances.GammaLScalarH2Surjectivity
+import GQ2.Dyadic.Instances.GammaLSimpleDirectSurjectivity
 import GQ2.Dyadic.Instances.GammaLActionImage
 import GQ2.Dyadic.Instances.GammaLActionImageDevissage
 import GQ2.Dyadic.Instances.GammaLAnalyticLeaves
