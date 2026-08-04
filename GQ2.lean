5707a0f504a7d35948c889defb293e34d36e98b5
David Roe <roed.math@gmail.com>
2026-08-04 03:31:51 -0400

    Prove finite two-group augmentation nilpotence

--- GQ2.lean
+++ GQ2.lean
@@ -412,6 +412,7 @@ import GQ2.Dyadic.Count.H3AugmentationFiltration
 import GQ2.Dyadic.Count.H3FiniteStronglyFreeNoGo
 import GQ2.Dyadic.Count.H3CompletedFoxReduction
 import GQ2.Dyadic.Count.H3AugmentationSeparation
+import GQ2.Dyadic.Count.FiniteTwoGroupAugmentationNilpotence
 import GQ2.Dyadic.Count.H3SqRowInitialForms
 import GQ2.Dyadic.Count.H3SqQuadraticPBW
 import GQ2.Dyadic.Instances.GammaLSylowRightExact
