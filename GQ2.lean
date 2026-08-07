6641bfdee52a072e6ce8584fcd2430936a98a016
David Roe <roed@mit.edu>
2026-08-07 07:20:52 -0400

    wire the class-two graded layer into the root

--- GQ2.lean
+++ GQ2.lean
@@ -695,6 +695,7 @@ import GQ2.Dyadic.SqCore.UVFrames
 import GQ2.Dyadic.SqCore.ArbFrames
 import GQ2.Dyadic.SqCore.CommFrames
 import GQ2.Dyadic.SqCore.EichRefutation
+import GQ2.Dyadic.SqCore.GradedTwo
 import GQ2.Dyadic.LabuteInterface
 import GQ2.Dyadic.OrientationCorrection
 import GQ2.Dyadic.NpcJet.Defs
