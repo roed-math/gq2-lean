b176759a0cf429f71891d149e2212e7324dbd37e
David Roe <roed@mit.edu>
2026-08-07 08:47:22 -0400

    wire the class-three graded layer into the root

--- GQ2.lean
+++ GQ2.lean
@@ -696,6 +696,7 @@ import GQ2.Dyadic.SqCore.ArbFrames
 import GQ2.Dyadic.SqCore.CommFrames
 import GQ2.Dyadic.SqCore.EichRefutation
 import GQ2.Dyadic.SqCore.GradedTwo
+import GQ2.Dyadic.SqCore.GradedThree
 import GQ2.Dyadic.LabuteInterface
 import GQ2.Dyadic.OrientationCorrection
 import GQ2.Dyadic.NpcJet.Defs
